40e180300d714905e603ae39325228f2bd9fc003
26Tanjiro <165337145+26Tanjiro@users.noreply.github.com>
2024-03-29 10:03:01 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Asses;
+
+internal static class Program
+{
+	 static void Main()
+	{
+
+   Console.WriteLine("Jake Joshua B. Peñalosa");
+   Console.Write("BSCpE 1-1");
+   Console.Write("\n");
+   
+  for (int i = 1; i <= 100; i++)
+  {
+  if (i % 3 == 0 && i % 5 == 0)
+ {
+  Console.WriteLine("Hello Goodbye");
+  }
+  else if (i % 3 == 0)
+  {
+  Console.WriteLine("Hello");
+  }
+  else if (i % 5 == 0)
+  {
+  Console.WriteLine("Goodbye");
+   }
+  else
+  {
+  Console.WriteLine(i);
+  }
+  }
+    }
+}
+
+	
